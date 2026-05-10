fd3fa80be8e20dc7d79b43ac10892df3a2099fd8
PiHost <i@pihost-dev.ru>
2026-05-10 23:27:32 +0300

    Intermediate file upload implementation

--- PIconMaker/listed_widget.cpp
+++ PIconMaker/listed_widget.cpp
@@ -1,14 +1,74 @@
 #include "listed_widget.h"
 #include "ui_listed_widget.h"
 
+#include <QFileDialog>
+#include <QStringList>
+#include <QIcon>
+#include <QPixmap>
+
 ListedWidget::ListedWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::ListedWidget)
-{
+    ui(new Ui::ListedWidget){
     ui->setupUi(this);
+
+    connect( ui->btn_add,       &QPushButton::clicked, this, &ListedWidget::onLoadClicked );
+    connect( ui->btn_remove,    &QPushButton::clicked, this, &ListedWidget::onSaveClicked );
+    connect( ui->btn_clear,     &QPushButton::clicked, this, &ListedWidget::onClearClicked );
+
+    connect( ui->w_iconsList, &QListWidget::currentItemChanged, this, &ListedWidget::onListIndexChanged );
+
 }
 
-ListedWidget::~ListedWidget()
-{
+ListedWidget::~ListedWidget(){
     delete ui;
+}
+
+void ListedWidget::onLoadClicked(){
+    QFileDialog loadDlg( this, tr( "Загрузить картинки" ) );
+    loadDlg.setFileMode( QFileDialog::FileMode::ExistingFiles );
+    loadDlg.setNameFilter( "Доступные типы картинок (*.png, *.bmp, *.jpg)" ); // TODO: fix
+    loadDlg.setNameFilters( { "*.png", "*.bmp", "*.ico" } );
+
+    if( loadDlg.exec() ){
+        QStringList pathes = loadDlg.selectedFiles();
+        for( const auto &file : qAsConst( pathes ) ){
+            if( !loadByPath( file ) ){
+                // TODO: inc
+            }
+        }
+    }
+    // TODO: fail signal
+}
+
+void ListedWidget::onSaveClicked(){
+
+}
+
+void ListedWidget::onClearClicked(){
+    if( !m_imagesList.isEmpty() ){
+        m_imagesList.clear();
+        ui->w_iconsList->clear();
+    }
+}
+
+void ListedWidget::onListIndexChanged( QListWidgetItem *item ){
+    QSize curRowSize = m_imagesList.at( ui->w_iconsList->currentRow() ).size();
+    ui->w_icon->setPixmap( QPixmap( item->icon().pixmap( curRowSize ) ) );
+}
+
+bool ListedWidget::loadByPath( const QString &path ){
+    QImage tmp( path );
+    if( tmp.width() == tmp.height() && tmp.width() <= 1024 ){
+        m_imagesList.append( tmp );
+
+        QPixmap tmpPxMp;
+        tmpPxMp.convertFromImage( tmp );
+        QListWidgetItem *item = new QListWidgetItem( QIcon( tmpPxMp ), QString( "%1x%2" ).arg( tmp.width() ).arg( tmp.height() ) );
+        item->setSizeHint( QSize( ui->w_iconsList->viewport()->width(), 120 ) );
+        ui->w_iconsList->addItem( item );
+
+        return true;
+    }
+
+    return false;
 }
--- PIconMaker/listed_widget.h
+++ PIconMaker/listed_widget.h
@@ -2,13 +2,18 @@
 #define LISTED_WIDGET_H
 
 #include <QWidget>
+#include <QList>
+#include <QImage>
+#include <QString>
+
+#include <QListWidget>
+#include <QListWidgetItem>
 
 namespace Ui {
 class ListedWidget;
 }
 
-class ListedWidget : public QWidget
-{
+class ListedWidget : public QWidget {
     Q_OBJECT
 
 public:
@@ -16,7 +21,17 @@ public:
     ~ListedWidget();
 
 private:
+    Q_SLOT void onLoadClicked();
+    Q_SLOT void onSaveClicked();
+    Q_SLOT void onClearClicked();
+
+    Q_SLOT void onListIndexChanged( QListWidgetItem *item );
+
+    bool loadByPath( const QString &path );
+
     Ui::ListedWidget *ui;
+
+    QList< QImage > m_imagesList;
 };
 
 #endif // LISTED_WIDGET_H
